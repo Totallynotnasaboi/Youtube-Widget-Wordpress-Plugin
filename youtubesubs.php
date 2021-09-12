4ef487e018e93e0c3a203ab1c29c598261e06e67
Francesco Silvani <83598456+Totallynotnasaboi@users.noreply.github.com>
2021-09-12 16:09:49 +0000

    Added style include

---
+++ youtubesubs.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: YouTube subscribe widget
+Description: A widget that displays channel name and icon + subcribe button
+Version:     0.1
+Author:      Francesco Silvani
+Author URI:  https://nasa-boi.ga
+License:     GPL2 etc
+*/
+
+// Exit if accesed
+if(!defined('ABSPATH')){
+  exit;
+}
+
+// Load Scripts
+require_once(plugin_dir_path(__FILE__). '/include/youtubesubs-scripts.php');
+
+?>
