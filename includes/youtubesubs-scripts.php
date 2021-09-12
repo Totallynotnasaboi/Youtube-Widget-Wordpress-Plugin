3ac55a44466b93e58fdc490ea34d1e11efbfb1ea
Francesco Silvani <83598456+Totallynotnasaboi@users.noreply.github.com>
2021-09-12 17:04:12 +0000

    Added google API for sub count

--- includes/youtubesubs-scripts.php
+++ includes/youtubesubs-scripts.php
@@ -6,6 +6,10 @@ function yts_add_scripts(){
   wp_enqueue_style('yts-main-style', plugins_url(), "/youtubesubs/css/style.css");
   // Add Main JS
   wp_enqueue_script('yts-main-script', plugins_url(), "/youtubesubs/js/main.js");
+
+  // Add Google Scripts
+  wp_register_script('google', 'https://apis.google.com/js/platform.js');
+  wp_enqueue_script('google');
 }
 
 add_action('wp_enqueue_scripts', 'yts_add_scripts');
